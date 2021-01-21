a2bcb85f0856b9a4573a9c3d144ebf144ebdd9fd
unknown <valmar_mmm@hotmail.com>
2021-01-21 19:15:33 -0300

    Somente começo

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float speed;
+    public Rigidbody2D rig;
+
+    // Start is called before the first frame update - é chamado uma vez ao inicializar o jogo
+    void Start() {
+        
+    }
+
+    // Update is called once per frame - é chamado a cada frame
+    void Update() {
+        
+    }
+
+    // Called when Physics 2D is altered. - é chamado pela física do jogo
+    void FixedUpdate(){
+        float direction = Input.GetAxis("Horizontal"); //horizontal input
+
+        rig.velocity = new Vector2(direction * speed, rig.velocity.y); // y with no speed - y sem velocidade
+
+        if(direction > 0) {
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+
+        if (direction < 0) {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+    }
+}
