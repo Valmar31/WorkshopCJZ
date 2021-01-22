b84fdc8a147dbf04cd6dff2193a03a785e18e5e8
unknown <valmar_mmm@hotmail.com>
2021-01-22 03:29:00 -0300

    Movimentação completa + ataque

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     public float speed;
+    public float jumpForce;
+
+    bool isJumping;
+    bool isAttacking;
+
     public Rigidbody2D rig;
     public Animator anim;
 
@@ -14,7 +19,38 @@ public class Player : MonoBehaviour {
 
     // Update is called once per frame - é chamado a cada frame
     void Update() {
-        
+        Jump();
+        OnAttack();
+    }
+
+    void Jump() {
+        if(Input.GetButtonDown("Jump")) { 
+
+            if (isJumping == false) { //(!isJumping)
+                anim.SetInteger("transition", 2);
+
+                rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);       //rig is the body, addForce is what give "strenght" to jump
+                // or ...(new Vector2(0,1) * jumpForce....)
+
+                isJumping = true; // can't jump again mid air
+            }
+            
+        }
+    }
+
+    void OnAttack() {
+        if(Input.GetButtonDown("Fire1")) {
+            isAttacking = true;
+            anim.SetInteger("transition", 3);
+
+            StartCoroutine(OnAttacking());
+        }
+    }
+
+    // coroutine
+    IEnumerator OnAttacking() {
+        yield return new WaitForSeconds(0.5f);
+        isAttacking = false;
     }
 
     // Called when Physics 2D is altered. - é chamado pela física do jogo
@@ -23,19 +59,26 @@ public class Player : MonoBehaviour {
 
         rig.velocity = new Vector2(direction * speed, rig.velocity.y); // y with no speed - y sem velocidade
 
-        if(direction > 0) {
+        if(direction > 0 && !isJumping && !isAttacking) {
             transform.eulerAngles = new Vector2(0, 0);
             anim.SetInteger("transition", 1);
         }
 
-        if (direction < 0) {
+        if (direction < 0 && !isJumping && !isAttacking) {
             transform.eulerAngles = new Vector2(0, 180);
             anim.SetInteger("transition", 1);
         }
 
-        if (direction == 0) {
+        if (direction == 0 && !isJumping && !isAttacking) {
             anim.SetInteger("transition", 0);
         }
     
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        //if = true, player is touching the ground
+        if (collision.gameObject.layer == 8) {
+            isJumping = false;
+        }
     }
 }
